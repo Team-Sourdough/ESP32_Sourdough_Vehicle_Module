5be7e4855f5ff312320fe37c40d02e5087659bbe
advi4972 <77562961+advi4972@users.noreply.github.com>
2023-02-21 10:27:17 -0700

    Av integrate event flags (#6)
    * feat: add event group declaration
    
    * feat: add event flag logic for setting, clearing and pending on event flags
    
    * fix: compilation errors
    
    * event flags working with GPS and button interrupt properly
    
    * fix: rf task recieving updated gps data and button interrupt working properly

--- lib/GPS/GPS.cpp
+++ lib/GPS/GPS.cpp
@@ -38,16 +38,14 @@ bool GPS_Location(uint8_t *array, uint32_t timer, Adafruit_GPS *GPS) {
             }
       }
 
-      //Todo: Refactor to use a semaphore timer instead!
-      //Timer to update twice per second
+            //Timer to update twice per second
       if (millis() - timer > 500) {
             //Reset the timer
             timer = millis();
-
-            //Print current Fix status to determine if we have a connection.
-            
             //GPS is active: Calculate lat and longitude
             if (GPS->fix==1) {
+                  Serial.println("Got a fix");
+                  Serial.println(GPS->antenna);
                   //Calculate correct degrees for lat and lat minutes
                   float latitude = GPS->latitude/100.0; // as DD.MMMM
                   int latDegreesInt = (int)(latitude); //DD
@@ -75,10 +73,10 @@ bool GPS_Location(uint8_t *array, uint32_t timer, Adafruit_GPS *GPS) {
                   longitude *= -1.0;
                   }
                   //Display Measured Latitude and Longitude
-                  // Serial.print("Latitude: ");
-                  // Serial.println(latitude, 8);
-                  // Serial.print("Longitude: ");
-                  // Serial.println(longitude, 8);
+                  Serial.print("Lat: ");
+                  Serial.println(latitude, 8);
+                  Serial.print("Long: ");
+                  Serial.println(longitude, 8);
 
                   //Put found latitude and longitude into array to be sent.
                   uint8_t* tempLat = (uint8_t*)(&latitude);
@@ -90,8 +88,12 @@ bool GPS_Location(uint8_t *array, uint32_t timer, Adafruit_GPS *GPS) {
                         array[i+4] = tempLong[i];
                   }
                   return true;
+            }else{ //TODO: Remove else, used for debugging
+                  Serial.println(GPS->antenna);
+                  Serial.println("Trying to get location");
             }
       }
+      
       //If we got here... fix was zero / GPS not establishing connection.
       return 0;
 }
@@ -168,6 +170,7 @@ void GPS_Task(void* p_arg){
       Serial.println("Got into GPS Task");
       Adafruit_GPS GPS(&GPSSerial);
       GPS_Setup(&GPS);
+      uint32_t timer = millis();
       //Serial.println("GPS setup");
       uint8_t data_array[DataBufferSize];
       int cont = 0;
@@ -176,27 +179,22 @@ void GPS_Task(void* p_arg){
 
       //Create the buffer that will be used to send data over to the RF task
       while(1){
-            //Get location and post semaphore for RF
+            //Get location and post event flag for RF
 
             //Returns true if we get a fix and data
             cont = GPS_Location(data_array,timer,&GPS);
 
+            if(cont){
+                  //To my knowledge, pack will get all our data into an array
+                  GPS_Pack_uint8(data_array, &GPS); //This will pack all of our info for the system
 
-            //if(cont) {
-            //To my knowledge, pack will get all our data into an array
-            GPS_Pack_uint8(data_array, &GPS); //This will pack all of our info for the system
+                  //This posts the data to the message buffer that the RF module will use
+                  Message_Buffer_Send(xMessageBuffer, data_array);
 
-            //This posts the data to the message buffer that the RF module will use
-            Message_Buffer_Send(xMessageBuffer, data_array);
-            //delay(100);
-           // }
+                  //Post updateGPS event flag
+                  xEventGroupSetBits(rfEventGroup, updateGPS);
+            }
 
-
-            //Message queue to RF 
-
-            //Post semaphore
-            Serial.println("GPS Task!");
-            delay(100);
       }
 
 }
--- lib/common.c
+++ lib/common.c
@@ -2,9 +2,9 @@
 
 #define BUTTON_GPIO 2
 
-void Message_Buffer_Recieve(MessageBufferHandle_t xMessageBuffer, uint8_t data_array[]){
+void Message_Buffer_Recieve(MessageBufferHandle_t xMessageBuffer, uint8_t data_array[]){ //TODO: May need to add a mutex to protect reading and writing to the 
       size_t xReceivedBytes;
-      xReceivedBytes = xMessageBufferReceive( xMessageBuffer, ( void * ) data_array, sizeof(*data_array),x100ms);
+      xReceivedBytes = xMessageBufferReceive( xMessageBuffer, ( void * ) data_array, DataBufferSize, x100ms);
       return;
 }
 
@@ -31,9 +31,9 @@ MessageBufferHandle_t Message_Buffer_Create_25byte(){
 void Message_Buffer_Send( MessageBufferHandle_t xMessageBuffer, uint8_t data_array[]){
       size_t xBytesSent;
 
-      xBytesSent = xMessageBufferSend(xMessageBuffer, ( void * ) data_array, sizeof(*data_array), x100ms);
+      xBytesSent = xMessageBufferSend(xMessageBuffer, ( void * ) data_array, DataBufferSize, x100ms);
 
-      if(xBytesSent != sizeof(*data_array)){
+      if(xBytesSent != DataBufferSize){
             // The call to xMessageBufferSend() times out before there was enough
             // space in the buffer for the data to be written.
             Serial.println("The buffer did not allocate space in time\n");
@@ -44,4 +44,21 @@ void Message_Buffer_Send( MessageBufferHandle_t xMessageBuffer, uint8_t data_arr
             Serial.println("We good\n");
       }
       return;
+}
+
+EventGroupHandle_t EventGroupCreate(){
+      // Attempt to create the event group.
+      EventGroupHandle_t xCreatedEventGroup = xEventGroupCreate();
+
+      // Was the event group created successfully?
+      if( xCreatedEventGroup == NULL )
+      {
+      // The event group was not created because there was insufficient
+      // FreeRTOS heap available.
+            while(1); //Get stuck if event group doesn't get created
+      }
+      else
+      {
+            return xCreatedEventGroup;
+      }
 }
