a6a002f8848af9298d1c587014050e58667c41d7
CyrusDS <david.brevik@gmail.com>
2023-03-11 16:35:39 -0700

    Merging to main

--- lib/Mic/Mic.h
+++ lib/Mic/Mic.h
@@ -3,9 +3,24 @@
 
 #include "../common.h"
 #include "../Button/Button.h"
+#include "../I2S_Driver/I2S.h"
+#include "../wav/wav.h"
+#include <driver/i2s.h>
+#include <FS.h>
+#include <SD.h>
+#include <SPI.h>
+
+#define SCK  17
+#define MISO  18
+#define MOSI  16
+#define CS  6
+#define I2S_MODE I2S_MODE_RX
+SPIClass spi = SPIClass(HSPI);
+char record_runs = '0';
 
 void mic_setup();
-
+void sd_setup();
+void button_setup();
 void Mic_Task(void* p_arg);
 
 
